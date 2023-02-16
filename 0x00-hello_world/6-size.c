e687a4822e4aa3db43529e807d2caa9050d6c831
Melbanda28 <melanbanda@gmail.com>
2023-02-16 12:57:13 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,6 @@ int main(void)
 	printf("Size of an int: 4 byte(s)\n", sizeof(int));
 	printf("Size of a long int: 4 byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: 8 byte(s)\n", sizeof(long long int));
-	printf("Sise of a float: 4 byte(s)\m", sizeof(float));
+	printf("Sise of a float: 4 byte(s)\n", sizeof(float));
 	return (0);
 }
