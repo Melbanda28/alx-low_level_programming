8961de02427eb38e956408445d29a9739665b7b0
Melbanda28 <melanbanda@gmail.com>
2023-02-21 12:58:14 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints 10 times the alphabet in lowercase
+ */
+
+void print_alphabet_x10(void);
+{
+	int ten;
+
+	char letter
+
+		for (ten = 0; ten <= 9; ten++)
+		{
+			for (letter = 'a'; la <= 'z'; la++)
+				_putchar(letter);
+
+			_putchar('\n');
+		}
+}
