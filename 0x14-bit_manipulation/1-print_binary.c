302fd2b228d6508d573c3072b7a623e5c093bbc7
Melbanda28 <melanbanda@gmail.com>
2023-04-08 01:59:32 -0700

    update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,17 +1,23 @@
 #include "main.h"
-#include <stdio.h>
 
 
 void print_binary(unsigned long int n) 
 {
-    int i;
+	int i, count = 0;
+	unsigned long int current;
 
-    for (i = (sizeof(unsigned long int) * 8) - 1; i >= 0; i--) 
-    {
-        if ((n >> i) & 1)
-            putchar('1');
-        else
-            putchar('0');
-    }
+	for (i = 63; i >= 0; i--)
+	{
+		current = n >> i;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
 }
-
